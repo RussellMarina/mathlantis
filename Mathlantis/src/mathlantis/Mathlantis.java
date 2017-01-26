8066b62c2ed44e885b03a01c82f2c150ceff364e
RussellMarina <marinairussell@gmail.com>
2017-01-26 10:45:19 -0500

    1/26/2017 - Group meeting

---
+++ Mathlantis/src/mathlantis/Mathlantis.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathlantis;
+
+import byui.cit260.mathlantis.model.Game;
+
+/**
+ *
+ * @author Marina
+ */
+public class Mathlantis {
+    
+    public static void main(String[] args){
+        Game gameOne = new Game();
+        
+        gameOne.setTotalTime(1.00);
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+    }
+}
